5ba12a1fbc42d54a8b4322febee9ff16e70ac04f
Ewan Chatham <echatham@outlook.com>
2024-07-26 01:41:57 -0700

    Acknowledgments kinda done

---
+++ script.js
@@ -0,0 +1,8 @@
+function showSection(sectionId) {
+    var sections = document.querySelectorAll('.section');
+    sections.forEach(function(section) {
+        section.classList.remove('active');
+    });
+    var activeSection = document.getElementById(sectionId);
+    activeSection.classList.add('active');
+}
